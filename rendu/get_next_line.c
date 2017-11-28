06fbf6283b37b6f3485cd67356cfde61fa46a3d1
Raphael KRIEF <rkrief@e2r6p3.42.fr>
2017-11-29 00:03:57 +0100

    coucou

--- rendu/get_next_line.c
+++ rendu/get_next_line.c
@@ -6,11 +6,26 @@
 /*   By: rkrief <rkrief@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/23 14:05:18 by rkrief            #+#    #+#             */
-/*   Updated: 2017/11/28 22:14:21 by rkrief           ###   ########.fr       */
+/*   Updated: 2017/11/29 00:02:13 by rkrief           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+char 	*ft_strjoinfree(char *s1, char *s2)
+{
+	char    *str;
+
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+	str = (char*)ft_memalloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (str == NULL)
+		return (NULL);
+	str = ft_strcat(str, s1);
+	str = ft_strcat(str, s2);
+//	free (s2);
+	return (str);
+}
 
 int	ft_error(int i, char **str, int j, char **line)
 {
@@ -23,13 +38,6 @@ int	ft_error(int i, char **str, int j, char **line)
 		**line = '\0';
 		return (0);
 	}
-	return (5);
-}
-
-int	ft_next(char **str, int j, char **line)
-{
-	int i;
-
 	i = 0;
 	while (str[j][i] != '\n' && str[j][i])
 		i++;
@@ -40,31 +48,60 @@ int	ft_next(char **str, int j, char **line)
 	return (1);
 }
 
+int	find_endl(char *buf, char **str, int j)
+{
+	size_t	i;
+
+	i = 0;
+	while (buf[i])
+	{
+		if (buf[i] == '\n')
+			break ;
+		i++;
+	}
+	if (i == ft_strlen(buf))
+		return (0);
+	if (str[j] == NULL)
+		str[j] = ft_strdup(buf);
+	else
+		str[j] = ft_strjoin(str[j], buf);
+	return (1);
+}
+
+
 int get_next_line(const int fd, char **line)
 {
 	int			j;
 	static char **str;
 	char		buf[BUFF_SIZE + 1];
 	int			i;
-	int			o;
-
+//	char		*clone;
 	if (str == NULL)
-		str = (char**)malloc(sizeof(char*) * 1000);
+		str = (char**)malloc(sizeof(char*) * 1000000);
 	j = fd;
 	if (fd < 0)
 		return (-1);
 	while ((i = read(fd, buf, BUFF_SIZE)) > 0)
 	{
 		buf[i] = '\0';
+		if (find_endl(buf, str, j))
+			break ;
 		i = ft_strlen(buf);
 		if (str[j] == NULL)
 			str[j] = ft_strdup(buf);
 		else	
+		{
 			str[j] = ft_strjoin(str[j], buf);
+//			clone  = ft_strjoin(str[j], buf);
+		
+//			free (clone);
+		}
 	}
-	o = ft_error(i, str, j, line);
-	if (o != 5)
-		return (o);
+	i = ft_error(i, str, j, line);
+	if (i == 0)
+		return (0);
+	if (i == -1)
+		return (-1);
 	return (1); 
 }
 /*
@@ -80,4 +117,4 @@ int main(int argc, char **argv)
 	while (get_next_line(fd, line))
 		ft_putstr(*line);
 	return (0);
-} */
+} */ 
